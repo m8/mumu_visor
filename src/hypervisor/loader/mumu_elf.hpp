d32248518b8faa33409da009f7eb9bafce228c8c
Musa Unal <umusasadik@gmail.com>
2021-09-14 09:08:45 +0300

    mumu_elf revised

--- src/hypervisor/loader/mumu_elf.hpp
+++ src/hypervisor/loader/mumu_elf.hpp
@@ -3,42 +3,40 @@
 #include <string>
 #include <vector>
 
-#include "../kvm/vm.h"
 #include "./elf.hpp"
+#include "../kvm/vm.h"
 #include "../../tools/exec.h"
 
-
-using namespace std;
 vector<elf_parser::section_t> secs;
 
-
-elf_parser::section_t get_section_from_name (string name,vector<elf_parser::section_t> & secs = secs) {
+// Finds and returns section from name
+elf_parser::section_t get_section_from_name (std::string name,vector<elf_parser::section_t> & secs = secs) {
     for(auto k: secs){
         if(k.section_name == name){
             return k;
         }
     }
+    return {};
 }
 
 // Get entry point of elf folder each load.
-long get_entry_address(string path){
-    string command = "readelf -h \""+path+"\" | grep Entry";
-    auto asd = exec(command.c_str());
-    auto q = asd.substr(asd.size()-7).substr(0, 7);
-    char *p;
-    long n = strtol( q.c_str(), & p, 16 );
-    return n;
+long get_entry_address(std::string path){
+    std::string command = "readelf -h \""+path+"\" | grep Entry";
+    auto cmd = exec(command.c_str());
+    auto parsed_address = cmd.substr(cmd.size()-7).substr(0, 7);
+    return (strtol(parsed_address.c_str(), NULL, 16 ));
 }
 
-
-void load_elf(string path, VM* vm){
-    elf_parser::Elf_parser parser(path);
-    vector<elf_parser::segment_t> segs = parser.get_segments();
-    secs = parser.get_sections();
+// Load elf file to vm memory region
+void load_elf(std::string path, VM* vm){
+    elf_parser::Elf_parser parser {path};
     auto map = parser.get_memory_map();
+    auto segs = parser.get_segments();
 
+    secs = parser.get_sections();
 
     char *text = (char *)vm->mem + 0x0000;
+
     for(auto sec: secs){
         memcpy(text + sec.section_offset,map + sec.section_offset, sec.section_size );
     }
