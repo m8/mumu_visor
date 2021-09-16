cf0c0b7d068290f6ddc0740a4c2a0df42530e490
Musa Unal <umusasadik@gmail.com>
2021-09-16 15:54:16 +0300

    VCPU header file revised

--- src/hypervisor/kvm/vcpu.hpp
+++ src/hypervisor/kvm/vcpu.hpp
@@ -15,7 +15,7 @@ using namespace std;
 class VCPU {
 public:
     // Constructors
-    VCPU(){};
+    VCPU() = default;
 
     VCPU(int, int, struct kvm_run*,int);
 
@@ -26,7 +26,6 @@ public:
     int run_real_mode();
     int run_protected_mode();
 
-
     void* kvm_cpu_thread(void *args);
 
     void run(int,int);
@@ -35,25 +34,21 @@ public:
 
     void set_cores(int, int);
 
-    void page_tables(void* mem, struct kvm_sregs *sregs);
+    void set_page_tables(void* mem, struct kvm_sregs *sregs);
     
-    void setup_segment_registers();
+    void set_segment_registers();
 
     void* (*vcpu_thread_func)(void *);    
 
     // Member Variables
+    int id, vcpu_fd, physical_core, virtual_core = 0;
+    bool lock = false; 
+  
     sem_t mtx;
     pthread_t vcpu_thread = 0;
     
-    bool lock = false; 
-    int physical_core = 0;
-    int virtual_core = 0;
-    int id = 0; 
-    
     kvm_sregs sregs; 
     kvm_regs regs;   
-
-    uint16_t vcpu_fd = 0;
 
     string buffer = "";
 
